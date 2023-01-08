596952ce11fa5ecafdaa7b54c96f00a6faefb720
Lukáš Marek <marl33@vse.cz>
2023-01-08 21:57:52 +0100

    Oprava SQL pro zobrazování přátel

--- src/main/java/com/damare/data/DbFunctions.java
+++ src/main/java/com/damare/data/DbFunctions.java
@@ -366,7 +366,7 @@ public class DbFunctions {
         ResultSet rs = null;
         try {
 
-            String query = String.format("select * from devel_user inner join devel_friendship  on devel_user.user_id=devel_friendship.user_id1_requester where devel_friendship.user_id2_requested=%s and devel_friendship.friendship_status_code=1", id);
+            String query = String.format("select fu.user_id,fu.username,fu.user_email,fu.user_passwd from devel_user u inner join devel_friendship f on (u.user_id=f.user_id1_requester or u.user_id=f.user_id2_requested) inner join devel_user fu on (fu.user_id=f.user_id1_requester or fu.user_id=f.user_id2_requested) where (u.user_id=%s) and f.friendship_status_code=1 and fu.user_id<>u.user_id", id);
             statement = conn.createStatement();
             rs = statement.executeQuery(query);
             Integer i = 0;
