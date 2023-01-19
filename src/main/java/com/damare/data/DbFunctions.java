60a9ab4d2e564e10846a43fa95246a7b2f0fc9b3
Lukáš Marek <marl33@vse.cz>
2023-01-19 20:45:36 +0100

    Změna readAllUsers na verzi, která zobrazuje uživatele bez přátel

--- src/main/java/com/damare/data/DbFunctions.java
+++ src/main/java/com/damare/data/DbFunctions.java
@@ -349,7 +349,7 @@ public class DbFunctions {
         ResultSet rs = null;
         try {
 
-            String query = String.format("select * from devel_user where not user_id= %s",id);
+            String query = String.format("with friends as (select fu.user_id from devel_user u inner join devel_friendship f on (u.user_id=f.user_id1_requester or u.user_id=f.user_id2_requested) inner join devel_user fu on (fu.user_id=f.user_id1_requester or fu.user_id=f.user_id2_requested) where (u.user_id=%s) and fu.user_id<>u.user_id) select * from devel_user u left join friends f on u.user_id=f.user_id where f.user_id is null and u.user_id<>%s",id);
             statement = conn.createStatement();
             rs = statement.executeQuery(query);
             Integer i = 0;
