09d2c8eec236c3db37a6eb44e297c0a45f9d937d
Pavel Kurek <kurp03@vse.cz>
2023-01-20 16:20:55 +0100

    done -> setup tests

---
+++ test/com/damare/main/HomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.damare.main;
+
+import com.damare.model.ApplicationState;
+import com.damare.model.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+
+    @Test
+    void loadMyCategories() {
+    }
+
+}
---
+++ test/com/damare/model/ControllerUtilsTest.java
@@ -0,0 +1,25 @@
+package com.damare.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerUtilsTest {
+
+    @Test
+    void addUser() {
+
+    }
+
+    @Test
+    void addTask() {
+    }
+
+    @Test
+    void viewAllUsers() {
+    }
+
+    @Test
+    void removeTask() {
+    }
+}
